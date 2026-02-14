4e778d4ccbdc7a8a48e2e05356d88766bb226524
rcjasub <jasub270@gmail.com>
2026-02-14 18:39:39 -0500

    first initial set up load

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import SnippetBlock from '../components/SnippetBlock';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Code Board MVP</h1>
+      <SnippetBlock />
+      {/* In the future, you can render multiple draggable blocks */}
+    </div>
+  );
+}
