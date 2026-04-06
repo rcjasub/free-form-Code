d8f49a2dbbd3a7e38fbafde4d26da4f955842626
rcjasub <jasub270@gmail.com>
2026-04-06 18:07:07 -0400

    added unit testing for getCanvasBySharedId

--- backend/__tests__/canvasController.test.ts
+++ backend/__tests__/canvasController.test.ts
@@ -1,4 +1,5 @@
-import { getCanvasById, createCanva } from "../controllers/canvasController";
+import { describe } from "node:test";
+import { getCanvasById, createCanva, getCanvasByShareId } from "../controllers/canvasController";
 
 import * as Canvas from "../models/canvas";
 
@@ -88,4 +89,65 @@ describe("createCanva", () => {
     expect(res.status).toHaveBeenLastCalledWith(201);
     expect(res.json).toHaveBeenCalledWith(fakeCanvas);
   });
+
+  test("retunrs 500 on database error", async () => {
+    mockCanvas.getById.mockRejectedValue(new Error("db error"));
+
+    await getCanvasById(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ error: "db error" });
+  });
+});
+
+describe("getCanvasByShareId", () => {
+  let req: any;
+  let res: any;
+
+  beforeEach(() => {
+    req = { params: { id: "1" } };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+  });
+
+  test("returns 200 and canvas when found", async () => {
+    const fakeCanvas = {
+      id: "1",
+      user_id: "2",
+      name: "My new Canvas",
+      share_id: "abc123",
+      is_public: true,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+
+    mockCanvas.getCanvasByShareId.mockResolvedValue(fakeCanvas);
+
+    await getCanvasByShareId(req, res);
+
+    expect(res.status).toHaveBeenLastCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(fakeCanvas);
+  });
+
+
+  test("returns 404 when shared canvas not found", async () => {
+    mockCanvas.getCanvasByShareId.mockResolvedValue(null);
+
+    await getCanvasByShareId(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ error: "Shared Canvas not found" });
+  })
+
+  test("retunrs 500 on database error", async () => {
+    mockCanvas.getCanvasByShareId.mockRejectedValue(new Error("db error"));
+
+    await getCanvasByShareId(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ error: "db error" });
+  });
+
 });
